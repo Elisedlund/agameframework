5e72b1c305cc00f1b331bd6c4815d48795192048
elisedlund@gmail.com <elisedlund@gmail.com@8867d858-669e-b7b3-7d94-6db262c3ced0>
2011-04-13 18:00:57 +0000

    *quick port a old java minesweeper game i made still some bugs and no flag.

---
+++ trunk/AGameFrameworkMines/src/com/agameframework/Settings.java
@@ -0,0 +1,23 @@
+package com.agameframework;
+
+import com.agameframework.elis.mines.GameRoot;
+import com.agameframework.interfaces.ISettings;
+import com.agameframework.settings.GameSettings;
+
+public class Settings implements ISettings{
+
+	@Override
+	public void loadSettings() {
+
+		//Here can you do some general settings with "GameSettings"
+		GameSettings.DEBUG_LEVEL1 = true; //enable printing to log.
+		GameSettings.SHOW_FPS = true;//show possible updates per second.
+		GameSettings.DEVELOPER_MAIL = "elisedlund@gmail.com";//set an email to enable error reporter.
+
+		
+		GameRoot game = new GameRoot(); //Creates the starting GameNode. 
+		Game.setLoader(game); //Sets the loader. Dont have to be the same as the GameNode(GameRoot)
+		Game.setGameRoot(game); //Sets the root GameNode
+	}
+
+}
